91d3d66d2b3a3d85b2ae0861dba040cb6848d0bc
Alex Darigan <47497501+AlexDarigan@users.noreply.github.com>
2024-04-29 04:04:17 +0100

    Add Inbox

--- src/pages/Inbox.js
+++ src/pages/Inbox.js
@@ -1,9 +1,48 @@
+import Panel from "../layout/Panel";
+import ScrollGrid from "../layout/ScrollGrid";
+import Column from "../layout/Column";
+import Row from "../layout/Row";
+import TextInput from "../components/TextInput";
+import Button from "../components/Button";
+import Label from "../components/Label";
+
+
 function Inbox() {
     return (
-        <div>
-            Inbox
-        </div>
+        <Panel width="800px">
+            <Row width="800px">
+                <ScrollGrid width="300px">
+                   <Message/>
+                   <Message/>
+                   <Message/>
+                   <Message/>
+                   <Message/>
+                   <Message/>
+                   <Message/>
+                   <Message/>
+                   <Message/>
+
+                </ScrollGrid>
+                <Column margin="8px" height="100%">
+                    <TextInput/>
+                    <textarea style={{width: "1000px", height: "300px"}}></textarea>
+                    <Button text="Send"/>   
+                </Column>
+            </Row>
+        </Panel>
     )
+}
+
+function Message() {
+    return (
+        <Panel width="280px" margin="8px" height="fit-content" alignHorizontal="left">
+        <Column alignHorizontal="left">
+            <Label label="Offer"/>
+            <Label label="@AlexD"/>
+            <Label label="27th March 2020"/>
+        </Column>
+        </Panel>
+    );
 }
 
 export default Inbox;
